d88d0caaee53eff8d8e027f9600513d8f7a478e0
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-10-22 15:29:47 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,8 +1,1 @@
-import os as alpha
-alpha.system("nvidia-smi")
-import ColabCode
-import os
-os.system("bash")
-os.system("pip install colabcode")
 from colabcode import ColabCode
-ColabCode(port=2010, password="123123")
